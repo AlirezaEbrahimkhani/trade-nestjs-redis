e60fddf5ec56052a3434fe52070c24f4cdd8d68f
AlirezaEbrahimkhani <alirezaebrahimkhanidev@gmail.com>
2022-10-28 21:40:22 +0330

    feat: create flush database function

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { CACHE_MANAGER, Controller, Delete, Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Delete('flush')
+  async flushDB() {
+    await this.cacheManager.reset();
   }
 }
