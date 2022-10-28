989c6ee32059d7e4b23f94a0fddad53050c62427
AlirezaEbrahimkhani <alirezaebrahimkhanidev@gmail.com>
2022-10-28 21:40:37 +0330

    feat: register modules and redisStore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SymbolModule } from './symbol/symbol.module';
+import { StackHolderModule } from './stack-holder/stack-holder.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      ttl: 10000,
+    }),
+    SymbolModule,
+    StackHolderModule,
+    TransactionModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
